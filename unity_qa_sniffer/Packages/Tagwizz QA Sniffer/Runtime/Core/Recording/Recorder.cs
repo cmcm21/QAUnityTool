64d6bce63897aebdfa786e14fd183133d26ab759
Miguel <miguel.correa@tagwizz.com.mx>
2023-08-04 12:35:35 -0600

    Recorder init

---
+++ unity_qa_sniffer/Packages/Tagwizz QA Sniffer/Runtime/Core/Recording/Recorder.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using TagwizzQASniffer.Core.InputSystem;
+
+namespace TagwizzQASniffer.Core.InputSystem
+{
+    public class Recorder
+    {
+        private SnifferInputSystem _inputSystem;
+    }
+}
