d88779eb1ef56966f35a834cef785b3612a6f07c
Miguel <miguel.correa@tagwizz.com.mx>
2023-09-06 18:05:22 -0600

    --Device disconnected manager from device

--- unity_qa_sniffer/Packages/Tagwizz QA Sniffer/Runtime/Exceptions/SnifferException.cs
+++ unity_qa_sniffer/Packages/Tagwizz QA Sniffer/Runtime/Exceptions/SnifferException.cs
@@ -22,17 +22,40 @@ namespace TagwizzQASniffer.Exceptions
         public SaveFileNetworkErrorException(string filePath) : base($"Error while sending file {filePath}") {}
     }
 
-    public class SnifferCoreLoadingFileError : Exception
+    public class SnifferCoreLoadFileException : Exception
     {
-        public SnifferCoreLoadingFileError(){}
-        public SnifferCoreLoadingFileError(string filePath): 
+        public SnifferCoreLoadFileException(){}
+        public SnifferCoreLoadFileException(string filePath): 
             base($"Sniffer core couldn't load file {filePath}, file is corrupted"){}
     }
 
-    public class SnifferCoreSavingFileError : Exception
+    public class SnifferCoreSaveFileException : Exception
     {
-        public SnifferCoreSavingFileError() {}
-        public SnifferCoreSavingFileError(string filePath):
+        public SnifferCoreSaveFileException() {}
+        public SnifferCoreSaveFileException(string filePath):
             base($"Sniffer core couldn't save file {filePath}"){}
+    }
+
+    public class NetworkServerConnectionErrorException : Exception
+    {
+        public NetworkServerConnectionErrorException(){}
+        
+        public NetworkServerConnectionErrorException(string address, int port):
+            base($"Connection to {address} in port : {port} wasn't made"){}
+    }
+
+    public class NetworkSendDataErrorException : Exception
+    {
+       public NetworkSendDataErrorException(){} 
+       public NetworkSendDataErrorException(byte[] data, string toIp) : 
+           base($"Exception trying to send bytes:{data.Length} to :{toIp}"){}
+    }
+
+    public class NetworkReceiveDataErrorException : Exception
+    {
+        public NetworkReceiveDataErrorException() { }
+        
+        public NetworkReceiveDataErrorException(string fromIp): 
+            base($"Exception trying to receive data from {fromIp} "){}
     }
 }
